23eb6edeff73cafeb01a45c0bded393aa935129a
mbrown-2 <michaelbrown.5619@gmail.com>
2024-08-12 21:15:59 -0700

    exploration of JQuery properties for custom caching/stale-data treatmentg

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,30 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
-import axios, { AxiosError } from "axios";
-import React, { useEffect, useState } from "react";
-
-interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
+import useTodos from "./hooks/useTodos";
 
 const TodoList = () => {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
-
   // Query object; destructure for the "data" property
-  const { data: todos, error } = useQuery<Todo[], Error>({
-    queryKey: ["todos"],
-    queryFn: fetchTodos,
-  });
+  const { data: todos, error, isLoading } = useTodos();
 
-  axios.AxiosError;
-
-  // Stuff above replaces state and effect hooks
-
+  if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
 
   return (
