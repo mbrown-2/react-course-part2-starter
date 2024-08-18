08092df325695068aa30ce744973c22d6855c38d
mbrown-2 <michaelbrown.5619@gmail.com>
2024-08-18 10:52:04 -0700

    error routes

--- src/routing/ErrorPage.tsx
+++ src/routing/ErrorPage.tsx
@@ -1,8 +1,19 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
+  const error = useRouteError();
+
+  // Differentiate between program and endpoint errors
+  const isRouteError = isRouteErrorResponse(error);
+
   return (
     <>
       <h1>Oops...</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        {isRouteError
+          ? "404 Page Not Found"
+          : "Sorry, an unexpected error has occurred."}
+      </p>
     </>
   );
 };
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -3,6 +3,7 @@ import HomePage from "./HomePage";
 import UserDetail from "./UserDetail";
 import Layout from "./Layout";
 import UsersPage from "./UsersPage";
+import ErrorPage from "./ErrorPage";
 
 // Provide an array of route objects : (path, element / a component to render)
 // A router renders components only when the user is able to view them.
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
