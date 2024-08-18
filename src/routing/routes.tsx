6f598ec296ab789052a1d6d5b2a21135c41a1bbb
mbrown-2 <michaelbrown.5619@gmail.com>
2024-08-18 11:19:22 -0700

    code experimentation

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -4,6 +4,8 @@ import UserDetail from "./UserDetail";
 import Layout from "./Layout";
 import UsersPage from "./UsersPage";
 import ErrorPage from "./ErrorPage";
+import LoginPage from "./LoginPage";
+import PrivateRoutes from "./PrivateRoutes";
 
 // Provide an array of route objects : (path, element / a component to render)
 // A router renders components only when the user is able to view them.
@@ -16,9 +18,15 @@ const router = createBrowserRouter([
     path: "/",
     element: <Layout />,
     errorElement: <ErrorPage />,
+    children: [{ index: true, element: <HomePage /> }],
+  },
+  {
+    // No path, is a "layout route"
+    // Purpose: group routes / enforce layouts and/or business rules.
+    element: <PrivateRoutes />,
     children: [
-      { index: true, element: <HomePage /> },
       {
+        // Placing as child will protect it
         path: "users",
         element: <UsersPage />,
         children: [{ path: ":id", element: <UserDetail /> }],
